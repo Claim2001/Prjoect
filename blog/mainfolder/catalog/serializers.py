c928600b53f4cbf92e353c85c7a3733ab382120d
Claim2001 <akmal.musaev.2001@mail.ru>
2020-09-22 10:35:42 +0500

    First commit

---
+++ blog/mainfolder/catalog/serializers.py
@@ -0,0 +1,44 @@
+from rest_framework import serializers
+from .models import Article, User
+
+
+class ArticleSerializer(serializers.Serializer):
+    article_title = serializers.CharField(max_length=200)
+    article_additional_title = serializers.CharField(max_length=400)
+    article_text = serializers.CharField()
+
+    def create(self, validated_data):
+        validated_data.get('')
+        return Article.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get("article_title", instance.title)
+        instance.additional_title = validated_data.get("article_additional_title", instance.additional_title)
+        instance.text = validated_data.get("article_text", instance.text)
+        instance.save()
+        return instance
+
+
+class ArticleCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = '__all__'
+
+
+class ArticleDeleteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = '__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'password')
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
